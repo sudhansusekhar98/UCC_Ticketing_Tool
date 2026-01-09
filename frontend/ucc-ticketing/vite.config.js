b74a32d01b489a77ce47b4e401ec4d346df69139
sudha-vlaccess25 <sekhar@vlaccess.com>
2026-01-09 18:10:02 +0530

    vite-config updated

--- frontend/ucc-ticketing/vite.config.js
+++ frontend/ucc-ticketing/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['lucide-react', 'react-hot-toast'],
+        },
+      },
+    },
+  },
 })
