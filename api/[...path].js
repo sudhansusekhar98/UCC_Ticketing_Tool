553b24d7be9b9a70cc66409b24218b1235de8fc7
sudha-vlaccess25 <sekhar@vlaccess.com>
2026-01-10 12:56:56 +0530

    update

--- api/[...path].js
+++
@@ -1,7 +0,0 @@
-import '../backend-express/server.js';
-
-export default async function handler(req, res) {
-  // This will be handled by the Express app
-  const app = (await import('../backend-express/server.js')).default;
-  return app(req, res);
-}
--- database/FixPasswords.sql
+++
@@ -1,9 +0,0 @@
--- Fix password hash for all users
--- Password: Admin@123
--- BCrypt Hash: $2a$11$hWFq47uN0H37eujUXiD.Ie6GbPWNo4cJDIDQlypLXDpgx9N7pq7Z.
-
-UPDATE Users 
-SET PasswordHash = '$2a$11$hWFq47uN0H37eujUXiD.Ie6GbPWNo4cJDIDQlypLXDpgx9N7pq7Z.'
-WHERE UserId >= 1;
-
-SELECT Username, PasswordHash FROM Users;
