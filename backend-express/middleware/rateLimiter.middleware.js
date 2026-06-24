ac3da778a1fddc667c5150d82113f8750da17547
sudha-vlaccess25 <sekhar@vlaccess.com>
2026-06-24 12:14:21 +0530

    rate limiter increased

--- backend-express/middleware/rateLimiter.middleware.js
+++ backend-express/middleware/rateLimiter.middleware.js
@@ -13,7 +13,7 @@ import rateLimit from 'express-rate-limit';
  */
 export const generalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
+    max: 500,
     message: {
         success: false,
         message: 'Too many requests from this IP, please try again after 15 minutes'
